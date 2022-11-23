2fdcc05ea988e59e8101aea57e8afca5b22aa87e
Gene Zhang <genezhang@gmail.com>
2022-11-23 15:55:25 -0800

    animated title card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback, useMemo } from 'react';
 
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import useLocalStorage from './misc/useLocalStorage';
@@ -15,6 +15,7 @@ import GameOverModal from './components/GameOverModal';
 import { DifficultySelection } from './components/DifficultySelection';
 import { useMutex } from './misc/useMutex';
 import { GameIdKeys, HintsByCorrectPosition, HintsByLetter, LetterGuess, LetterState, ResponseError } from './misc/types';
+import { GameInfoSource, TitleCard } from './components/TitleCard';
 
 const VERSION = "0.1.3"
 const WORD_LEN = 5;
@@ -47,6 +48,7 @@ function App() {
    const [isHardMode, setIsHardMode] = useLocalStorage<boolean>(localKey("isHardMode"), true);
 
    // These states DO NOT persist across sessions
+   const [gameSource, setGameSource] = useState<GameInfoSource>('localStorage')
    const [buffer, setBuffer] = useState("");
    // Prevent multiple simultaneous API calls with a semaphore
    const mutex = useMutex()
@@ -61,6 +63,7 @@ function App() {
          setBuffer("");
          setGuesses([]);
          setAnswers(null)
+         setGameSource('newGame')
 
          setGameIdKeys(gameInfo)
       })()
@@ -176,7 +179,7 @@ function App() {
 
    return renderWhen(gameIdKeys,
       (<div className="App">
-         <Title>Wordeau #{gameIdKeys?.id}</Title>
+         <TitleCard {...{ id: gameIdKeys?.id || 0, source: gameSource }} />
          <DifficultySelection {...{ isHardMode, setIsHardMode }} />
          <GuessBoard {...{ guesses, buffer }} />
          <KeyboardHints {...{ letterHints }} />
